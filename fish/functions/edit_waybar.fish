17fd672d6f6144fdeb44de8297234a7d11c35856
Antonio <antoniofs23@gmail.com>
2024-02-05 02:44:05 -0500

    added config updater

---
+++ fish/functions/edit_waybar.fish
@@ -0,0 +1,4 @@
+function edit_waybar --wraps='nvim ~/.config/waybar/' --description 'alias edit_waybar=nvim ~/.config/waybar/'
+  nvim ~/.config/waybar/ $argv
+        
+end
---
+++ fish/functions/update_configs.fish
@@ -0,0 +1,5 @@
+function update_configs
+	cd ~/.config/
+	cp -r -v kitty/ fish/ waybar/ wofi/ hypr/ ~/Archyprdots
+	cd ~
+end
