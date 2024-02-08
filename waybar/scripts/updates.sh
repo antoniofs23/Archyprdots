2777478f32a37a0d511e10e4b2981a8931f1234e
Antonio <antoniofs23@gmail.com>
2024-02-08 18:29:00 -0500

    automated-updating

--- waybar/scripts/updates.sh
+++ waybar/scripts/updates.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+#  _   _           _       _             
+# | | | |_ __   __| | __ _| |_ ___  ___  
+# | | | | '_ \ / _` |/ _` | __/ _ \/ __| 
+# | |_| | |_) | (_| | (_| | ||  __/\__ \ 
+#  \___/| .__/ \__,_|\__,_|\__\___||___/ 
+#       |_|                              
+
+# ----------------------------------------------------- 
+# Define threshholds for color indicators
+# ----------------------------------------------------- 
 
 threshhold_green=0
 threshhold_yellow=25
 threshhold_red=100
 
 # ----------------------------------------------------- 
-# Calculate available updates pacman and aur (with trizen)
+# Calculate available updates via yay call
 # ----------------------------------------------------- 
+updates=$(yay -Syup | grep http:// | wc -l)
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
-
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
-
-updates=$(("$updates_arch" + "$updates_aur"))
+# ----------------------------------------------------- 
+# Output in JSON format for Waybar Module custom-updates
+# ----------------------------------------------------- 
 
 css_class="green"
 
@@ -33,3 +38,4 @@ if [ "$updates" -gt $threshhold_green ]; then
 else
     printf '{"text": "0", "alt": "0", "tooltip": "0 Updates", "class": "green"}'
 fi
+
