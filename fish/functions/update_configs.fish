3ccaf5b716a9a5fc1c333f0c3e2b91464fa2a06d
Antonio Fernandez <antoniofs23@gmail.com>
2024-02-07 22:39:10 -0500

    swapped from cp to rysnc

--- fish/functions/update_configs.fish
+++ fish/functions/update_configs.fish
@@ -1,7 +1,7 @@
 function update_configs
 	# copy all config files to git repo
 	cd ~/.config/
-	rsync -auv kitty/ fish/ waybar/ wofi/ hypr/ nvim/ ~/Archyprdots
+	rsync -auvP kitty/ fish/ waybar/ wofi/ hypr/ nvim/ ~/Archyprdots
   pacman -Q > ~/Archyprdots/logs/packages.txt
 
   # push to github
