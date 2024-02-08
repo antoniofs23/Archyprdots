46851b372966fe438d3321d0fd9d089c32aa48b0
Antonio Fernandez <antoniofs23@gmail.com>
2024-02-07 22:36:31 -0500

    Update update_configs.fish

--- fish/functions/update_configs.fish
+++ fish/functions/update_configs.fish
@@ -1,7 +1,7 @@
 function update_configs
 	# copy all config files to git repo
 	cd ~/.config/
-	cp -r -v kitty/ fish/ waybar/ wofi/ hypr/ nvim/ ~/Archyprdots
+	rsync -auv kitty/ fish/ waybar/ wofi/ hypr/ nvim/ ~/Archyprdots
   pacman -Q > ~/Archyprdots/logs/packages.txt
 
   # push to github
